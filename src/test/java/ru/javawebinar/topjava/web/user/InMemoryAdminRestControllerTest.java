1019cbd3a1a848ae196901a0b8faf793a552b115
Timofei <timofei725@yandex.ru>
2022-11-05 23:39:35 +0300

    hw3 full

--- src/test/java/ru/javawebinar/topjava/web/user/InMemoryAdminRestControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/user/InMemoryAdminRestControllerTest.java
@@ -3,6 +3,7 @@ package ru.javawebinar.topjava.web.user;
 import org.junit.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.javawebinar.topjava.repository.inmemory.InMemoryUserRepository;
@@ -13,11 +14,13 @@ import java.util.Arrays;
 import static ru.javawebinar.topjava.UserTestData.NOT_FOUND;
 import static ru.javawebinar.topjava.UserTestData.USER_ID;
 
+
 @Ignore
 public class InMemoryAdminRestControllerTest {
     private static final Logger log = LoggerFactory.getLogger(InMemoryAdminRestControllerTest.class);
 
     private static ConfigurableApplicationContext appCtx;
+    @Autowired
     private static AdminRestController controller;
     private static InMemoryUserRepository repository;
 
